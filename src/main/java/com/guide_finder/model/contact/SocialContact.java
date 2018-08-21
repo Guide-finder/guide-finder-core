6ebc1076089162209d4f83b39c8e5e37884fd499
Dmytro Selentsov <dselentsov@danavero.com>
2018-08-21 11:18:42 +0300

    added getters and setters for social contacts

--- src/main/java/com/guide_finder/model/contact/SocialContact.java
+++ src/main/java/com/guide_finder/model/contact/SocialContact.java
@@ -4,10 +4,26 @@ public class SocialContact {
 
     private String link;
 
-    SocialContact type;
+    private SocialContact type;
 
     public SocialContact(String link, SocialContact type) {
         this.link = link;
+        this.type = type;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public SocialContact getType() {
+        return type;
+    }
+
+    public void setType(SocialContact type) {
         this.type = type;
     }
 }
