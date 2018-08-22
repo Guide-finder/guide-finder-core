162b705e95b6ed21207d47923200d60b68ce3996
stepanegorov <stepae@list.ru>
2018-08-22 17:00:26 +0300

    доделал вывод категорий

--- src/main/java/com/guide_finder/servlet/category/CategoriesServlet.java
+++ src/main/java/com/guide_finder/servlet/category/CategoriesServlet.java
@@ -38,31 +38,3 @@ public class CategoriesServlet extends HttpServlet {
         }
     }
 }
-
-
-//import com.guide_finder.service.abstraction.category.CategoryService;
-//import com.guide_finder.service.impl.category.CategoryServiceImpl;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-
-//@WebServlet("/user/category")
-//public class CategoriesServlet extends HttpServlet
-//{
-//    private final CategoryService categoryService = new CategoryServiceImpl();
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String text = "some text";
-//
-//        String json = new Gson().toJson(categoryService.getAllCategories());
-//
-//        resp.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
-//        resp.setCharacterEncoding("UTF-8"); // You want world domination, huh?
-//        resp.getWriter().write(json);
-//    }
-//}
