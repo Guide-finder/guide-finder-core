7c10844a726244eb22db2ca5201e7a5c59c2af5c
MaxSolovyev <m-solovyev@yandex.ru>
2018-08-22 16:55:50 +0300

    add LangugeDao

--- src/main/java/com/guide_finder/service/impl/LanguageServiceImpl.java
+++ src/main/java/com/guide_finder/service/impl/LanguageServiceImpl.java
@@ -1,27 +1,29 @@
 package com.guide_finder.service.impl;
 
 import com.guide_finder.dao.abstraction.LanguageDao;
+import com.guide_finder.dao.impl.LanguageDaoImpl;
 import com.guide_finder.model.user.Language;
 import com.guide_finder.service.abstraction.LanguageService;
+import com.guide_finder.util.DBHelper;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class LanguageServiceImpl implements LanguageService {
 
-    private final LanguageDao userDao;
+    private final LanguageDao languageDao;
 
     private static volatile LanguageServiceImpl instance;
 
     private LanguageServiceImpl() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
-        this.userDao = new UserDaoFactory().getUserDao();
+        languageDao = new LanguageDaoImpl(DBHelper.getConnection());
     }
 
-    public static UserServiceImpl getInstance() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+    public static LanguageServiceImpl getInstance() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
         if (instance == null) {
             synchronized (UserServiceImpl.class) {
                 if (instance == null) {
-                    instance = new UserServiceImpl();
+                    instance = new LanguageServiceImpl();
                 }
             }
         }
@@ -30,31 +32,31 @@ public class LanguageServiceImpl implements LanguageService {
 
     @Override
     public Language getLanguageById(long id) {
-        return null;
+        return languageDao.getLanguageById(id);
     }
 
     @Override
     public Language getLanguageByName(String name) {
-        return null;
+        return languageDao.getLanguageByName(name);
     }
 
     @Override
     public void saveLanguage(Language language) {
-
+        languageDao.saveLanguage(language);
     }
 
     @Override
     public void editLanguage(Language language) {
-
+        languageDao.editLanguage(language);
     }
 
     @Override
     public void remove(long id) {
-
+        languageDao.remove(id);
     }
 
     @Override
     public List<Language> getAllUsers() {
-        return null;
+        return languageDao.getAllLanguages();
     }
 }
